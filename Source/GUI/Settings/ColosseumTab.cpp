b45e32a1d240d32a0b9d7ccf6c4d61a1f8745f62
aldelaro5 <aldelaro5@gmail.com>
2018-07-05 12:34:25 -0400

    GUI/Settings: Switch the order of the Colosseum starters tabs
    Espeon is more important so it should be the first tab because it will be the one selected by default.

--- Source/GUI/Settings/ColosseumTab.cpp
+++ Source/GUI/Settings/ColosseumTab.cpp
@@ -6,11 +6,11 @@ ColosseumTab::ColosseumTab(QWidget* parent) : QWidget(parent)
 {
   m_starterTabs = new QTabWidget();
 
-  m_predictorFiltersUmbreon = new CommonPredictorFiltersWidget(this);
-  m_starterTabs->addTab(m_predictorFiltersUmbreon, tr("Umbreon"));
-
   m_predictorFiltersEspeon = new CommonPredictorFiltersWidget(this);
   m_starterTabs->addTab(m_predictorFiltersEspeon, tr("Espeon"));
+
+  m_predictorFiltersUmbreon = new CommonPredictorFiltersWidget(this);
+  m_starterTabs->addTab(m_predictorFiltersUmbreon, tr("Umbreon"));
 
   QVBoxLayout* mainLayout = new QVBoxLayout;
   mainLayout->addWidget(m_starterTabs);
